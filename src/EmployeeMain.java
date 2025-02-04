84f7f4706bd9ae63ad308da128acede57e2db2e1
SadikshyaAcharya39 <acharya.sadikshya33@gmail.com>
2025-02-04 23:34:04 +0545

    Updated

--- src/EmployeeMain.java
+++ src/EmployeeMain.java
@@ -1,10 +1,10 @@
-//public class EmployeeMain {
-//    public static void main(String[] args) {
-////        Employee emp = new Employee();
-////        emp.emp_id = 10;
-////        emp.emp_name = "Sadikshya Acharya";
-////        emp.age = 20;
-////        emp.display();
-//
-//    }
-//}
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.emp_id = 10;
+        emp.emp_name = "Sadikshya Acharya";
+        emp.age = 20;
+        emp.display();
+
+    }
+}
---
+++ src/PersonMain.java
@@ -0,0 +1,32 @@
+public class PersonMain {
+    public static void main(String[] args) {
+
+        Person person = new Person("Sadikshya", "Acharya", 20, "Kupondole");
+
+//        System.out.println(person.getFirstName());
+//        System.out.println(person.getLastName());
+//        System.out.println(person.getAddress());
+//        System.out.println(person.getAge());
+
+        System.out.println(person);
+
+
+
+        // Without using constructor
+
+        // Person person = new Person();
+
+//        person.setFirstName("Sadikshya");
+//        System.out.println(person.getFirstName());
+//
+//        person.setLastName("Acharya");
+//        System.out.println(person.getLastName());
+//
+//        person.setAge(20);
+//        System.out.println(person.getAge());
+//
+//        person.setAddress("Kupondole");
+//        System.out.println(person.getAddress());
+
+    }
+}
